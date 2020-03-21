1f50199978d6dc3315740bab890287349839f9d6
oriharel <ori.harel@gmail.com>
2020-03-21 16:56:16 +0200

    dfd

---
+++ precache-manifest.bbdcc19d4aab80707e5bbd5ff22cfa33.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb15f64e0b2e852e1bf9876efc9d8010",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f266636d28ed4f15e4ca",
+    "url": "/static/css/main.44991acf.chunk.css"
+  },
+  {
+    "revision": "a9c10b4429b9f5075ad8",
+    "url": "/static/js/2.578d5c2a.chunk.js"
+  },
+  {
+    "revision": "c9235f4eba250bf5f1756c64b0b5e8c4",
+    "url": "/static/js/2.578d5c2a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f266636d28ed4f15e4ca",
+    "url": "/static/js/main.1bf963b4.chunk.js"
+  },
+  {
+    "revision": "ded6d55ad132eb36e316",
+    "url": "/static/js/runtime-main.b822f51a.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/static/media/logo.ee7cd8ed.svg"
+  }
+]);
